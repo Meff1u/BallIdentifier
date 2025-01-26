a39b8c9ea3bdd451546966c5405bb1694a3faf91
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2025-01-26 13:59:25 +0100

    ui update

--- js/script.js
+++ js/script.js
@@ -8,15 +8,19 @@ document.getElementById('fileInput').addEventListener('change', function() {
 
 function showLoadingPopup() {
     const loadingPopup = document.getElementById('loadingPopup');
+    const overlay = document.getElementById('overlay');
     loadingPopup.style.display = 'flex';
+    overlay.style.display = 'block';
     loadingPopup.style.animation = 'fadeIn 0.5s';
 }
 
 function hideLoadingPopup() {
     const loadingPopup = document.getElementById('loadingPopup');
+    const overlay = document.getElementById('overlay');
     loadingPopup.style.animation = 'fadeOut 0.5s';
     setTimeout(() => {
         loadingPopup.style.display = 'none';
+        overlay.style.display = 'none';
     }, 500);
 }
 
@@ -24,11 +28,13 @@ function showResultPopup(country) {
     const resultPopup = document.getElementById('resultPopup');
     const resultTitle = document.getElementById('resultTitle');
     const resultImage = document.getElementById('resultImage');
+    const overlay = document.getElementById('overlay');
 
     resultTitle.textContent = `${country}`;
     resultImage.src = `assets/balls/${country}.png`;
 
     resultPopup.style.display = 'block';
+    overlay.style.display = 'block';
     resultPopup.style.animation = 'fadeIn 0.5s';
 }
 
@@ -36,9 +42,11 @@ document.getElementById('closeResultPopup').addEventListener('click', hideResult
 
 function hideResultPopup() {
     const resultPopup = document.getElementById('resultPopup');
+    const overlay = document.getElementById('overlay');
     resultPopup.style.animation = 'fadeOut 0.5s';
     setTimeout(() => {
         resultPopup.style.display = 'none';
+        overlay.style.display = 'none';
     }, 500);
 }
 
