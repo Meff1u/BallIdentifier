a63d1d22354b69a4537f8750a931645be5fc9ea1
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2025-01-26 13:21:51 +0100

    and again

--- functions/compareImage.js
+++ functions/compareImage.js
@@ -5,7 +5,7 @@ const path = require('path');
 exports.handler = async (event) => {
     // console.log('Event:', event);
     const { file } = JSON.parse(event.body);
-    console.log(fs.readdirSync(path.join('./functions/')));
+    console.log(fs.readdirSync(path.join('../')));
     const buffer = Buffer.from(file, 'base64');
     const ballsDir = path.join('balls');
     const ballFiles = fs.readdirSync(ballsDir);
