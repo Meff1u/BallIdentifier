1bcbe3a04305c4be55366838eeba5f581f273143
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2025-01-30 13:56:52 +0100

    changelog arrange

--- js/script.js
+++ js/script.js
@@ -43,9 +43,8 @@ function checkFileSize(file) {
             const canvas = document.createElement('canvas');
             const ctx = canvas.getContext('2d');
 
-            const aspectRatio = img.height / img.width;
             canvas.width = 1000;
-            canvas.height = 1000 * aspectRatio;
+            canvas.height = 1000 * (img.height / img.width);
 
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
